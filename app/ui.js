a67739fe37744e3e416e04ae0eee531682d96d12
Tim Laurent <90380834+Tim-CoditBE@users.noreply.github.com>
2022-10-25 17:07:06 +0200

    Add files via upload

--- app/ui.js
+++ app/ui.js
@@ -11,7 +11,7 @@ function showWelcomeMessage(username) {
     cardDiv.style.display = 'initial';
     welcomeDiv.innerHTML = `Welcome ${username}`;
     signInButton.setAttribute("onclick", "signOut();");
-    signInButton.setAttribute('class', "btn btn-success");
+    signInButton.setAttribute('class', "log");
     signInButton.innerHTML = "<span style='color:#0098CC'>Sign Out</span>";
 }
 
