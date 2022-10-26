df7a959ff74e73216f462705134b88684cedf0d6
Tim Laurent <90380834+Tim-CoditBE@users.noreply.github.com>
2022-10-26 14:32:28 +0200

    Add files via upload

--- app/ui.js
+++ app/ui.js
@@ -3,6 +3,7 @@ const welcomeDiv = document.getElementById("WelcomeMessage");
 const signInButton = document.getElementById("SignIn");
 const signOutButton = document.getElementById("SignOut");
 const cardDiv = document.getElementById("card-div");
+const cardDivmain = document.getElementById("card-divmain");
 const mailButton = document.getElementById("readMail");
 const profileButton = document.getElementById("seeProfile");
 const profileDiv = document.getElementById("profile-div");
@@ -10,7 +11,7 @@ const profileDiv = document.getElementById("profile-div");
 function showWelcomeMessage(username) {
     // Reconfiguring DOM elements
     cardDiv.style.display = 'initial';
-    cardDivmain.style.display = 'none';
+    cardDivmain.style.display = 'initial';
     welcomeDiv.innerHTML = `Welcome ${username}`;
     signInButton.setAttribute("onclick", "signOut();");
     signInButton.innerHTML = "Sign Out";
