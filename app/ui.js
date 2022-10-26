4dd3e914d535202db41254d4d8b8c87458de7322
Tim Laurent <90380834+Tim-CoditBE@users.noreply.github.com>
2022-10-26 11:31:06 +0200

    Add files via upload

--- app/ui.js
+++ app/ui.js
@@ -1,6 +1,7 @@
 // Select DOM elements to work with
 const welcomeDiv = document.getElementById("WelcomeMessage");
 const signInButton = document.getElementById("SignIn");
+const signOutButton = document.getElementById("SignOut");
 const cardDiv = document.getElementById("card-div");
 const cardDivmain = document.getElementById("card-divmain");
 const mailButton = document.getElementById("readMail");
